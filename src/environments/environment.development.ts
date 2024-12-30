c46c2c7ad9e47d838e527d890164eab90e60dd6f
Jaafar-Wannous <jaafarwannous1@gmail.com>
2024-12-30 03:03:09 +0200

    Remove environments folder and update .gitignore

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const enviroment = {
-  production: true,
-  firebaseApiKey: 'AIzaSyDx89XTiHjIR3jBfR-TTF47FbtmIwFEjcA'
-}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const enviroment = {
-  production: false,
-  firebaseApiKey: 'AIzaSyDx89XTiHjIR3jBfR-TTF47FbtmIwFEjcA'
-}
